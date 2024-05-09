b31b3a85fb925f016c4fa0343de987632d50d293
Aykut Mürkit <ayktmrkt@gmail.com>
2024-05-09 21:39:12 +0300

    ActionMethods example

---
+++ Test/Test.Web/Controllers/OrnekController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        // Bu bir ActionResult'dır
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return RedirectToAction("Index"); // ikinci parametresi Controller alıyor ama farklı bir controllera göndermek istersem ("index","Ornek") yapabilirsin.
+        }
+
+
+
+
+        // bir contentresult yazalım
+        //bunu da url ye yazarsan sana bir string döner sadece
+        //https://localhost:44332/Ornek/contentresult yazarsan gelir .
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+
+        //https://localhost:44332/Ornek/jsonresult
+        public IActionResult JsonResult()
+        {
+            return Json(new {Id=1,name="kalem"}); // isimsiz bir class oluşturuyoruz.
+        }
+
+        //hiçbir şey dönmüyorum.
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult(); 
+        }
+    }
+}
