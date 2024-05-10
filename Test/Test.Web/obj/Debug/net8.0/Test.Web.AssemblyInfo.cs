d42abafd1b6896fea283cb2d50cb7f68a1f49da4
aykutmurkit <ayktmrkt@gmail.com>
2024-05-10 10:38:34 +0300

    View
    Layout Sayfaları Oluşturmak

--- Test/Test.Web/obj/Debug/net8.0/Test.Web.AssemblyInfo.cs
+++ Test/Test.Web/obj/Debug/net8.0/Test.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee016d6d89242d0ca7bd605f29df06f0f5cfad5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34ec45b3883448b5213c98ddf846f696c311321")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
